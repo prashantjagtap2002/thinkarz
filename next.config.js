7917f07bcd71aa8641b458b5245fa5ffabcf617f
prashant3250 <prashantjagtap3250@gmail.com>
2026-07-21 18:58:41 +0530

    perf: add preconnect hints, modern browserslist, cache headers, and image config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  compress: true,
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -14,7 +15,14 @@ const nextConfig = {
         hostname: 'picsum.photos',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
     ],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
 };
 
