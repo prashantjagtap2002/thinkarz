c4880d9a80f722a8c6e1dc64d3f7f7f70386b4ff
prashant3250 <prashantjagtap3250@gmail.com>
2026-08-10 13:42:44 +0530

    Update official calling number to 8097444826

--- lib/structuredData.ts
+++ lib/structuredData.ts
@@ -7,7 +7,7 @@ export function generateOrganizationSchema() {
     logo: 'https://thinkarz.com/icon.png',
     description:
       'THINKARZ - trusted pre-owned cars, transparent deals. Buy, sell, service and test drive quality used cars in Mumbai.',
-    telephone: '+91 99999 99999',
+    telephone: '+91 80974 44826',
     email: 'info.thinkarz@gmail.com',
     address: {
       '@type': 'PostalAddress',
